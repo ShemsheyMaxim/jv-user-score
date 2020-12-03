97abf392c11136e4397ec57ec11fa4427055a95a
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-03 19:24:44 +0200

    rewrote method getUserScore with use split()

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,8 +5,9 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
     public int getUserScore(String[] records, String email) {
         for (String user : records) {
-            if (user.length() != email.length() && user.startsWith(email)) {
-                return Integer.parseInt(user.substring(email.length() + 1));
+            String [] emailAndScoreForUser = user.split(":");
+            if (emailAndScoreForUser.length == 2 && emailAndScoreForUser[0].equals(email)) {
+                return Integer.parseInt(emailAndScoreForUser[1]);
             }
         }
         throw new UserNotFoundException("User with given email doesn't exist");
